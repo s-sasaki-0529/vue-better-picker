5e168ae9c595c509bf31c5e53256902769509aab
Shingo.sasaki <shingo.sasaki.0529@gmail.com>
2022-01-22 22:11:06 +0900

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  plugins: ["vue"],
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
+};

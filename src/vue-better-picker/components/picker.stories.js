5f56f351f593abab159c513bb49cec20f0e54eb9
Shingo.sasaki <shingo.sasaki.0529@gmail.com>
2022-01-22 19:36:59 +0900

    add storybook demo

--- src/vue-better-picker/components/picker.stories.js
+++ src/vue-better-picker/components/picker.stories.js
@@ -1,5 +1,5 @@
 import picker from "./picker.vue";
-export default { title: "sample" };
+export default { title: "picker", component: picker };
 
 const Template = (args, { argTypes }) => ({
   components: { picker },
@@ -7,9 +7,16 @@ const Template = (args, { argTypes }) => ({
   template: `
     <div>
       <button @click="show">show</button>
-      <picker ref="picker" :data="[[['aaa', 'bbb'], ['ccc', 'ddd'], ['eee','fff']]]"/>
+      <pre v-if="state">{{ state }}</pre>
+      <picker ref="picker" @select="(...args) => state = args" v-bind="$props" />
     </div>
   `,
+  data: () => ({
+    state: null,
+  }),
+  mounted() {
+    this.show();
+  },
   methods: {
     show() {
       this.$refs.picker.show();
@@ -17,4 +24,41 @@ const Template = (args, { argTypes }) => ({
   },
 });
 
-export const Default = Template.bind({});
+const commonData = [
+  { text: "AAA", value: "aaa" },
+  { text: "BBB", value: "bbb" },
+  { text: "CCC", value: "ccc" },
+  { text: "DDD", value: "ddd" },
+  { text: "EEE", value: "eee" },
+  { text: "FFF", value: "fff" },
+  { text: "GGG", value: "ggg" },
+];
+
+const commonProps = {
+  cancelTxt: "cancel",
+  confirmTxt: "confirm",
+};
+
+export const Single = Template.bind({});
+Single.args = {
+  ...commonProps,
+  title: "single",
+  data: [commonData],
+  selectedIndex: [4],
+};
+
+export const Double = Template.bind({});
+Double.args = {
+  ...commonProps,
+  title: "double",
+  data: [commonData, commonData],
+  selectedIndex: [3, 4],
+};
+
+export const Triple = Template.bind({});
+Triple.args = {
+  ...commonProps,
+  title: "triple",
+  data: [commonData, commonData, commonData],
+  selectedIndex: [2, 3, 4],
+};

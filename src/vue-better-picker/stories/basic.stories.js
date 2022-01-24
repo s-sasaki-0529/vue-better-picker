4c9f88dd85a79aab7c6d3b93743283da397bebfd
Shingo.sasaki <shingo.sasaki.0529@gmail.com>
2022-01-24 23:50:19 +0900

    update basic story

--- src/vue-better-picker/stories/basic.stories.js
+++ src/vue-better-picker/stories/basic.stories.js
@@ -1,5 +1,5 @@
 import picker from "../components/picker.vue";
-export default { title: "picker", component: picker };
+export default { title: "Basic", component: picker };
 
 const Template = (args) => ({
   components: { picker },
@@ -11,25 +11,25 @@ const Template = (args) => ({
       <button @click="show = true">Show</button>
       <div>
         <h2>change event parameter</h2>
-        <div>{{ currentValues }}</div>
+        <div>{{ currentState }}</div>
       </div>
       <div>
         <h2>select event parameter</h2>
-        <div>{{ selectedValues }}</div>
+        <div>{{ selectedState }}</div>
       </div>
 
       <picker
         v-model="show"
-        @change="(index, value) => currentValues = { index ,value }"
-        @select="(...result) => selectedValues = result"
+        @change="(state) => currentState = state"
+        @select="(state) => selectedState = state"
         v-bind="args"
       />
     </div>
   `,
   data: () => ({
     show: true,
-    currentValues: null,
-    selectedValues: null,
+    currentState: null,
+    selectedState: null,
   }),
 });
 

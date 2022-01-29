eddb52c8c4192a4a835709a6ba8eef83cc347840
Shingo.sasaki <shingo.sasaki.0529@gmail.com>
2022-01-29 15:41:47 +0900

    remove storybook

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  framework: "@storybook/vue3",
-  core: {
-    builder: "webpack5",
-  },
-  stories: ["../src/vue-better-picker/stories/*.stories.@(js|ts)"],
-  addons: ["@storybook/addon-controls"],
-};

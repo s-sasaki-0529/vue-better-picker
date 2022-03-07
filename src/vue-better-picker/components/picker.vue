d43f8b0740d00092566da7ccf771b9bc2a55aa13
Shingo.sasaki <shingo.sasaki.0529@gmail.com>
2022-03-07 13:10:13 +0900

    centering wheels

--- src/vue-better-picker/components/picker.vue
+++ src/vue-better-picker/components/picker.vue
@@ -283,6 +283,7 @@ export default {
       padding: 0 16px;
       justify-content: space-around;
       .wheel {
+        width: 100%;
         height: 173px;
         overflow: hidden;
         font-size: $fontsize-large-xx;
